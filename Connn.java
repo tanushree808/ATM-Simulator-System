af78a05511a8c998cdd8deb6d665c8e4313add51
shree0603 <125433760+tanushree808@users.noreply.github.com>
2024-05-14 22:15:30 +0530

    Create  Connn.java

---
+++ Connn.java
@@ -0,0 +1,16 @@
+package bank.management.system;
+
+import java.sql.*;
+
+public class Connn {
+    Connection connection;
+    Statement statement;
+    public Connn(){
+        try{
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/bankSystem","root","AyushVish");
+            statement = connection.createStatement();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+}
